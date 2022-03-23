1609c25153dbe9d2535b32596ab5e0cf1c324458
luke <luke@thesandylizard.com>
2022-03-23 13:09:49 -0700

    n/a

--- install_my_environment.bash
+++ install_my_environment.bash
@@ -147,7 +147,7 @@ mylog REMOVENANC=$REMOVENANC
 
 ### Start Payload Section
 # these lines skip the payload if -c or --commit is ommitted
-#[ $COMMIT ]&& COMMAND || echo 'Skipping due to NO-COMMIT:'
+#[ $COMMIT ]&& COMMAND || echo '*** Skipping due to NO-COMMIT:'
     
 	  #TODO: add verbosity to let users know what is going on
     #mylog 'Verbose Mode Active'
@@ -156,7 +156,7 @@ mylog REMOVENANC=$REMOVENANC
     if [ -f "$ZSHRCBACKUPFILE" ]; then      
       if [ $FORCE = true ]; then
         mylog "$ZSHRCBACKUPFILE exists but you used --force, copying."
-        [ $COMMIT = true ] && /bin/cp -rf $ZSHRC $ZSHRCBACKUPFILE || mylog "Skipping due to NO-COMMIT: Not Overwriting - $ZSHRCBACKUPFILE" 
+        [ $COMMIT = true ] && /bin/cp -rf $ZSHRC $ZSHRCBACKUPFILE || mylog "*** --- Skipping due to NO-COMMIT: Not Overwriting - $ZSHRCBACKUPFILE" 
       else
         mylog "$ZSHRCBACKUPFILE exists. Please use --force to overwrite"
         mylog 'Exiting without safe .zshrc backup'
@@ -164,7 +164,7 @@ mylog REMOVENANC=$REMOVENANC
       fi
     else 
       mylog "$ZSHRCBACKUPFILE does not exist, copying."
-      [ $COMMIT = true ] && /bin/cp $ZSHRC $ZSHRCBACKUPFILE || echo "Skipping due to NO-COMMIT: /bin/cp $ZSHRC $ZSHRCBACKUPFILE" 
+      [ $COMMIT = true ] && /bin/cp $ZSHRC $ZSHRCBACKUPFILE || echo "*** -- Skipping due to NO-COMMIT: /bin/cp $ZSHRC $ZSHRCBACKUPFILE" 
       exit
     fi
     # end backup .zshrc
@@ -175,19 +175,19 @@ mylog REMOVENANC=$REMOVENANC
       if [ -d "~/.oh-my-zsh" ];then #if the .oh-my-zsh directory is not there, install it.
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
       else #if the .oh-my-zsh directory exists it's already installed
-        mylog 'Skipping due to EXISTS: downloading and executing Oh My Zsh intall script'
+        mylog '*** --- Skipping due to EXISTS: downloading and executing Oh My Zsh intall script'
       fi
     else
-      mylog 'Skipping due to NO-COMMIT: downloading and executing Oh My Zsh intall script'
+      mylog '*** -- Skipping due to NO-COMMIT: downloading and executing Oh My Zsh intall script'
     fi
     # end install oh_my_zsh
 
     # add support for oh_my_zsh plugins
     mylog 'adding support for oh_my_zsh plugins'
     if [[ $COMMIT = true ]];then
-      sed -i 's/plugins=(git)/plugins=(aliases brew common-aliases docker emoji extract gh git history iterm2 macos pip redis-cli sudo ubuntu ufw vscode web-search wp-cli xcode)/g' $ZSHRC || echo 'Skipping due to NO-COMMIT: Enabling Plugins'
+      sed -i 's/plugins=(git)/plugins=(aliases brew common-aliases docker emoji extract gh git history iterm2 macos pip redis-cli sudo ubuntu ufw vscode web-search wp-cli xcode)/g' $ZSHRC || echo '*** -- Skipping due to NO-COMMIT: Enabling Plugins'
     else 
-      mylog 'Skipping due to NO-COMMIT: adding support for oh_my_zsh plugins'
+      mylog '*** -- Skipping due to NO-COMMIT: adding support for oh_my_zsh plugins'
     fi
     # end add oh_my_zsh plugins
 
@@ -198,19 +198,19 @@ mylog REMOVENANC=$REMOVENANC
       if [[ $COMMIT = true ]];then
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10 >/dev/null 
       else  
-        mylog 'Skipping due to NO-COMMIT: Cloning powerlevel10k'
+        mylog '*** --- Skipping due to NO-COMMIT: Cloning powerlevel10k'
       fi  
     else
-        mylog "Skipping due to EXISTS: powerlevel10k already present in ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10"
+        mylog "*** --- Skipping due to EXISTS: powerlevel10k already present in ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10"
     fi
     mylog 'updating .zshrc with powerlevel10k'
     if [[ $COMMIT = true ]];then
       sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/g" $ZSHRC 
     else
-      mylog 'Skipping due to NO-COMMIT: updating .zshrc with powerlevel10k'
+      mylog '*** -- Skipping due to NO-COMMIT: updating .zshrc with powerlevel10k'
     fi
     mylog 'copying powerlevel10k config'
-    [ $COMMIT = true ]&& cp .p10k.zsh ~/ || mylog 'Skipping due to NO-COMMIT: Copying p10k configuration'
+    [ $COMMIT = true ]&& cp .p10k.zsh ~/ || mylog '*** - Skipping due to NO-COMMIT: Copying p10k configuration'
     # end install powerlevel10k
 
     # add nanc.function to $ZSHRC
@@ -220,12 +220,68 @@ mylog REMOVENANC=$REMOVENANC
         nanc_lines=$(cat nanc.function)
         awk -v addpattern="$nanc_lines" '/# User configuration/{print $0 "\n" addpattern;next}1' $ZSHRC > $ZSHRC.tmp && mv $ZSHRC.tmp $ZSHRC
       else
-        mylog "Skipping due to NO-COMMIT: add nanc.function to $ZSHRC"
+        mylog "*** --- Skipping due to NO-COMMIT: add nanc.function to $ZSHRC"
       fi
     else
-        mylog "Skipping due to EXISTS: nanc.function is already present in $ZSHRC"
+        mylog "*** --- Skipping due to EXISTS: nanc.function is already present in $ZSHRC"
     fi
 
     # end add nanc.function
+
+    # add locale infomation to to $ZSHRC
+    #export LANG=en_US.UTF-8
+    if ! grep -q "export LANG=en_US.UTF-8" "$ZSHRC"; then # String NOT found in .zshrc
+      mylog "add locale infomation 'export LANG=en_US.UTF-8' to to $ZSHRC"
+      if [[ $COMMIT = true ]];then # check COMMIT flag: true
+        sed -i '1i\export LANG=en_US.UTF-8' $ZSHRC # add line at top of .zshrc to comply with powerlevel10k
+      else # check COMMIT flag: false
+        mylog "*** --- Skipping due to NO-COMMIT: add locale 'export LANG=en_US.UTF-8' to $ZSHRC"
+      fi
+    else # String 'export LANG' found in .zshrc
+      # get the offending line, the previous grep run was quiet.
+      result=`grep "export LANG=en_US.UTF-8" "$ZSHRC"`
+      if [[ $result == \#* ]]; then # check if the line found is commented
+        mylog "result is commented, uncomment it."
+        if [[ $COMMIT = true ]];then # check COMMIT flag: true
+          sed -i '/export LANG=en_US.UTF-8/s/^#//g' $ZSHRC
+          mylog "'export LANG=en_US.UTF-8' is now uncommented."
+        else # check COMMIT flag: false
+          mylog "*** --- Skipping due to NO-COMMIT: uncommenting 'export LANG=en_US.UTF-8' in $ZSHRC"
+        fi
+      else # line found is uncommented
+          mylog "'export LANG=en_US.UTF-8' is uncommented."
+      fi
+      result=`grep "export LANG" "$ZSHRC"`
+        mylog "*** --- Skipping due to EXISTS: loacle information 'export LANG' is already present in $ZSHRC."
+        mylog "Can not add 'LANG=en_US.UTF-8', please add manually to $ZSHRC."
+    fi
+
+    #export LC_ALL=en_US.UTF-8
+    if ! grep -q "export LC_ALL=en_US.UTF-8" "$ZSHRC"; then # String NOT found in .zshrc
+      mylog "add locale infomation 'export LC_ALL=en_US.UTF-8' to to $ZSHRC"
+      if [[ $COMMIT = true ]];then # check COMMIT flag: true
+        sed -i '1i\export LC_ALL=en_US.UTF-8' $ZSHRC # add line at top of .zshrc to comply with powerlevel10k
+      else # check COMMIT flag: false
+        mylog "*** --- Skipping due to NO-COMMIT: add locale 'export LC_ALL=en_US.UTF-8' to $ZSHRC"
+      fi
+    else # String 'export LANG' found in .zshrc
+      # get the offending line, the previous grep run was quiet.
+      result=`grep "export LC_ALL=en_US.UTF-8" "$ZSHRC"`
+      if [[ $result == \#* ]]; then # check if the line found is commented
+        mylog "result is commented, uncomment it."
+        if [[ $COMMIT = true ]];then # check COMMIT flag: true
+          sed -i '/export LC_ALL=en_US.UTF-8/s/^#//g' $ZSHRC
+          mylog "'export LC_ALL=en_US.UTF-8' is now uncommented."
+        else # check COMMIT flag: false
+          mylog "*** --- Skipping due to NO-COMMIT: uncommenting 'export LC_ALL=en_US.UTF-8' in $ZSHRC"
+        fi
+      else # line found is uncommented
+          mylog "'export LC_ALL=en_US.UTF-8' is uncommented."
+      fi
+      result=`grep "export LANG" "$ZSHRC"`
+        mylog "*** --- Skipping due to EXISTS: loacle information 'export LANG' is already present in $ZSHRC."
+        mylog "Can not add 'LANG=en_US.UTF-8', please add manually to $ZSHRC."
+    fi
+    # end add locale infomation to to $ZSHRC
 
 ### End Payload Section 
