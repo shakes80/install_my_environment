6e221517c2be2e7f1cf578a46f9450ae7cfccccd
luke <luke@kvm1.sandylizardmerch.com>
2022-03-23 09:54:19 -0700

    Deleted test_script.bash from the git repository

--- test_script.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#nanc_lines=$(cat nanc.function)
-#echo "$nanc_lines"
-
-#awk -v addpattern="$nanc_lines" '/\# User configuration/{print $0 "\n" addpattern;next}1' ~/.zshrc
-
-#sed -e '${startLineNum},${endLineNum}d' ~/.zshrc
