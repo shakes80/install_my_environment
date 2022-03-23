fc5f6f59e94e7f3ae016bb7c3cb294f674faaf5b
luke <luke@thesandylizard.com>
2022-03-23 14:21:36 -0700

    n/a

--- install_my_environment.bash
+++ install_my_environment.bash
@@ -204,11 +204,12 @@ mylog REMOVENANC=$REMOVENANC
       mylog '*** -- Skipping due to NO-COMMIT: adding support for oh_my_zsh plugins'
     fi
     # end add oh_my_zsh plugins
+if [ ! -d "$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then echo "DIR not there"; else echo "DIR there";fi
 
     # clone and install powerlevel10k
     
     mylog 'cloning powerlevel10k'
-    if [ ! -d "$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then # if directory exists
+    if [ ! -d "$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then # if directory does not exist
       if [[ $COMMIT = true ]];then
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10 >/dev/null 
       else  
